cf9285e3068e144bc01dfceabcb9df958ac375f1
Srihith25 <dhulipalasrihith@gmail.com>
2025-12-09 00:41:00 -0600

    Updated TripMap

--- my-app/components/TripMap.tsx
+++ my-app/components/TripMap.tsx
@@ -1,35 +1,36 @@
 'use client';
 
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
-import { forwardRef, useImperativeHandle } from 'react';
-import L from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { Map as LeafletMap } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 interface TripMapProps {
-  activities: any[];
-  selectedActivity: any;
-  onSelectActivity: (a: any) => void;
-  isFullscreen: boolean;
+  activities: { coordinates: [number, number]; place: string }[];
   className?: string;
 }
 
-const TripMap = forwardRef(({ activities, selectedActivity, onSelectActivity, className }: TripMapProps, ref) => {
-  const defaultPosition: [number, number] =
-    activities.length > 0 ? activities[0].coordinates : [40.7128, -74.0060];
+const TripMap = forwardRef<{ getMap: () => LeafletMap | null }, TripMapProps>(
+  ({ activities, className }: TripMapProps, ref) => {
+    const defaultPosition: [number, number] =
+      activities.length > 0 ? activities[0].coordinates : [40.7128, -74.0060];
 
-  const map = useMap();
+    const mapRef = useRef<LeafletMap | null>(null);
 
-  useImperativeHandle(ref, () => ({
-    getMap: () => map,
-  }));
+    useImperativeHandle(ref, () => ({
+      getMap: () => mapRef.current,
+    }));
 
-  return (
-    <div className="relative w-full h-full">
+    return (
       <MapContainer
         center={defaultPosition}
         zoom={13}
-        scrollWheelZoom
+        scrollWheelZoom={true}
         className={className}
+        ref={(map) => {
+          // Store the map instance in the ref
+          if (map) mapRef.current = map;
+        }}
       >
         <TileLayer
           attribution="&copy; Stadia Maps"
@@ -37,17 +38,14 @@ const TripMap = forwardRef(({ activities, selectedActivity, onSelectActivity, cl
         />
 
         {activities.map((activity, idx) => (
-          <Marker
-            key={idx}
-            position={activity.coordinates}
-          >
+          <Marker key={idx} position={activity.coordinates}>
             <Popup>{activity.place}</Popup>
           </Marker>
         ))}
       </MapContainer>
-    </div>
-  );
-});
+    );
+  }
+);
 
 TripMap.displayName = 'TripMap';
 export default TripMap;
